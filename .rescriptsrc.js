b0a6e8f0122367c44d2f7665255aee3f5938d431
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2019-01-30 19:29:25 +0200

    Add monaco-editor-webpack-plugin

---
+++ .rescriptsrc.js
@@ -0,0 +1,13 @@
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
+module.exports = [
+  (config) => {
+    config.plugins.push(
+        new MonacoWebpackPlugin({
+          // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+          languages: ['json', 'typescript']
+        })
+    );
+    return config;
+  }
+];
